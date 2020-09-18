a7c24f53da1cb4a85c1d4649ccc667de2ccc2c18
Juho Inkinen <juho.inkinen@helsinki.fi>
2020-09-18 11:34:54 +0300

    Again thinner border in preview pics (to 4 px)

--- img/make-video-preview-images-from-presentations.sh
+++ img/make-video-preview-images-from-presentations.sh
@@ -18,6 +18,6 @@ mv ../presentations/*.png .
 for pngfile in *-1.png; do
 	echo $pngfile
     composite -gravity Center -quality 100 200px-YouTube_play_buttom_icon_\(2013-2017\).svg.png $pngfile $pngfile
-	convert -border 6 -bordercolor black -resize 30% $pngfile $pngfile
+	convert -border 4 -bordercolor black -resize 30% $pngfile $pngfile
 	#convert -border 20 -bordercolor black -resize 50% $pngfile $pngfile
 done
